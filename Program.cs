a40dc6117c8d30e031072e05ca45aeb57be1d615
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2020-12-08 14:06:58 +0100

    Small program to show that machine keys are incorrectly reported as user keys on Platform Crypto Provider

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace MachineKeyError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keyParams = new CngKeyCreationParameters
+            {
+                ExportPolicy = CngExportPolicies.None,
+                KeyCreationOptions = CngKeyCreationOptions.MachineKey, 
+                Provider = new CngProvider("Microsoft Platform Crypto Provider"),
+            };
+
+            CngKey key = CngKey.Create(CngAlgorithm.Rsa, $"TPM-Import-Key", keyParams);
+
+            Console.WriteLine($"Is the key a machine key? {key.IsMachineKey}");
+
+            key.Delete();   // just for cleanup, not necessary for reproduction
+        }
+    }
+}
